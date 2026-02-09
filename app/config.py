5c8c6e5bfc27b464cf7f011ce8d8b51ca7590832
Kadyr <Nekketsu4@mail.ru>
2026-02-09 23:23:51 +0300

    feat: added extra configuration data

--- app/config.py
+++ app/config.py
@@ -1,17 +1,23 @@
 import os
+from loguru import logger
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    # путь к БД
-    # DATABASE_URL: str
-    # TEST_DB_URL: str
+    # БД
     POSTGRES_HOST: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
     POSTGRES_PORT: int
+
+    # Путь к тестоовому БД
+    TEST_DB_URL: str
+
+    # Логирование
+    FORMAT_LOG: str
+    LOG_ROTATION: str
 
     # Настройки безопасности
     SECRET_KEY: str
@@ -21,6 +27,11 @@ class Settings(BaseSettings):
     DEBUG: bool
     PROJECT_NAME: str
     VERSION: str
+
+    # REDIS
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_DECODE_RESPONSE: bool
 
     @property
     def get_db(self) -> str:
@@ -35,3 +46,13 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+
+# Настройка логирования
+log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs.log.txt")
+logger.add(
+    log_file_path,
+    format=settings.FORMAT_LOG,
+    level="INFO",
+    rotation=settings.LOG_ROTATION,
+)
